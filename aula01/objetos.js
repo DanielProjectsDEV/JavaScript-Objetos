5a0331e25675d3ab70204b6fee982d443847180c
DanielProjectsDEV <developer.danielalves@gmail.com>
2024-01-01 21:57:46 -0300

    Estrutura de Dados de um Objeto em Js

---
+++ aula01/objetos.js
@@ -0,0 +1,11 @@
+const listaCPFs = [1111111, 22222, 3333]
+
+const informaçoesPessoa = ["nome", "José", "Idade", 32, "CPF", "1111222333"]
+
+console.log(informaçoesPessoa[1])
+
+const objetoPessoa = {    //ESTRUTURA DE DADOS DE UM OBJETO
+    nome: "José",
+    idade: 32,
+    CPF: "1111222333"
+}
