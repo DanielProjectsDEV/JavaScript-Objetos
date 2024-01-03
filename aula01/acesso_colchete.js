732cbcea54be22ae56d8811041d59f16e073b4ca
DanielProjectsDEV <developer.danielalves@gmail.com>
2024-01-03 20:54:29 -0300

    acessando dados com colchetes

---
+++ aula01/acesso_colchete.js
@@ -0,0 +1,8 @@
+const cliente = {
+    nome: "Andre",
+    idade: 32,
+    cpf: "1122233345",
+    email: "andre@dominio.com"
+}
+
+console.log(`O nome desse cliente é ${cliente["nome"]} e ele tem ${cliente["idade"]} Anos`)
